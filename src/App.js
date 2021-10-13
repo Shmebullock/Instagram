073dc5baee8a46cb4a167ececf8cfe2be7304076
Shmebullock <felixpsantana610@gmail.com>
2021-10-13 19:21:20 -0400

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Login = lazy(() => import('./pages/login'));
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path="ROUTES.LOGIN" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,18 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+/* eslint-disable no-unused-vars */
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
-// here is where I want to import the seed file (once)
-
-const config = {};
+const config = {
+  apiKey: 'AIzaSyAYLGY9jwObVDu_ka1jAOS40Vths8OjBSk',
+  authDomain: 'instagram-a1e97.firebaseapp.com',
+  projectId: 'instagram-a1e97',
+  storageBucket: 'instagram-a1e97.appspot.com',
+  messagingSenderId: '194524050080',
+  appId: '1:194524050080:web:cf9498c9871f65e7e8cfc5'
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
-
-// here is where I want to call the seed file (once)
-// seedDataBase
 
 export { firebase, FieldValue };
